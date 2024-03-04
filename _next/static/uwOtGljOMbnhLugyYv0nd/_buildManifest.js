e7360caa583616e717f9228cdbbcad3e7d335eab
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2024-03-04 17:44:48 +0100

    ooops go back

---
+++ _next/static/uwOtGljOMbnhLugyYv0nd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/381-8e2d810a74d6f83b.js","static/chunks/pages/index-2ef36ae8fc61c00e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
